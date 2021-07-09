5513c8782bd209eee2239242deb66c5e737c43b2
Jason Florentino <jasonmflorentino@gmail.com>
2021-07-09 16:01:09 -0400

    Further refinements - ready to try on Heroku

--- bot.js
+++ bot.js
@@ -1,18 +1,22 @@
 require("dotenv").config();
 const twitter_stream = require("./lib/twitter_stream");
+const config = require("./lib/config");
 
 async function run() {
   console.log("Starting bot")
-  // let currentRules;
-  try {
-    // currentRules = await twitter_stream.getAllRules();
-    // await twitter_stream.deleteAllRules(currentRules);
-    // await twitter_stream.setRules();
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
 
+  if (config.RESET_RULES_ON_STARTUP) {
+    let currentRules;
+    try {
+      currentRules = await twitter_stream.getAllRules();
+      await twitter_stream.deleteAllRules(currentRules);
+      await twitter_stream.setRules();
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
+  }
+  
   twitter_stream.streamConnect(0);
   console.log(`Stream created and listening...`);
 }
